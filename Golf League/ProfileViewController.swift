867bb28157bc438c866dd815c344ca0c752084cd
AJ Motika <andrew.motika@gm.com>
2017-12-21 08:32:20 -0500

    Initial commit setting up iOS project

---
+++ Golf League/ProfileViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileViewController.swift
+//  Golf League
+//
+//  Created by Andrew Motika on 12/21/17.
+//  Copyright © 2017 Andrew Motika. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    //MARK: Properties
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
