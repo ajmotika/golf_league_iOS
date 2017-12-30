2d3246d1750915f3093e4861ddcc9fdfd1765a1b
AJ Motika <andrew.motika@gm.com>
2017-12-30 16:19:22 -0500

    Added start round scene and configured scorecard tab to automatically go to start round scene when no round has been started

--- Golf League/controller/scorecard/CurrentRoundViewController.swift
+++ Golf League/controller/scorecard/CurrentRoundViewController.swift
@@ -21,9 +21,14 @@ class CurrentRoundViewController: UIViewController, UITableViewDataSource, UITab
     //MARK: UIViewController methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         scorecardTableView.dataSource = self
-        preloadRound()
+        //preloadRound()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if round == nil {
+            self.performSegue(withIdentifier: "ScorecardToStartRoundSegue", sender: self)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -66,6 +71,18 @@ class CurrentRoundViewController: UIViewController, UITableViewDataSource, UITab
         updateScores()
     }
 
+    //MARK: Actions
+    @IBAction func unwindToCurrentRound(sender: UIStoryboardSegue) {
+        if let startRoundViewController = sender.source as? StartRoundViewController {
+            round = startRoundViewController.round
+            self.scorecardTableView.reloadData()
+        }
+    }
+    
+    @IBAction func unwindToCurrentRoundFromCancel(sender: UIStoryboardSegue) {
+       
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -78,39 +95,12 @@ class CurrentRoundViewController: UIViewController, UITableViewDataSource, UITab
     
     //MARK: Private methods
     private func updateScores() {
-        guard let round = round else {
-            print("round not set")
-            return
+        if let round = round {
+            numStrokesLabel.text = String(round.strokes)
+            scoreLabel.text = round.score
         }
-        
-        numStrokesLabel.text = String(round.strokes)
-        scoreLabel.text = round.score
     }
     
-    private func preloadRound() {
-        let frontNine = NineHoles(
-            hole1: Hole(number: 1, par: 4, yardage: 399, handicap: 4),
-            hole2: Hole(number: 2, par: 3, yardage: 173, handicap: 16),
-            hole3: Hole(number: 3, par: 4, yardage: 331, handicap: 18),
-            hole4: Hole(number: 4, par: 4, yardage: 368, handicap: 8),
-            hole5: Hole(number: 5, par: 5, yardage: 501, handicap: 12),
-            hole6: Hole(number: 6, par: 4, yardage: 329, handicap: 6),
-            hole7: Hole(number: 7, par: 3, yardage: 163, handicap: 14),
-            hole8: Hole(number: 8, par: 5, yardage: 518, handicap: 10),
-            hole9: Hole(number: 9, par: 4, yardage: 456, handicap: 2))
-        
-        let backNine = NineHoles(
-            hole1: Hole(number: 10, par: 4, yardage: 431, handicap: 3),
-            hole2: Hole(number: 11, par: 3, yardage: 165, handicap: 13),
-            hole3: Hole(number: 12, par: 4, yardage: 390, handicap: 15),
-            hole4: Hole(number: 13, par: 4, yardage: 396, handicap: 11),
-            hole5: Hole(number: 14, par: 5, yardage: 547, handicap: 7),
-            hole6: Hole(number: 15, par: 5, yardage: 517, handicap: 9),
-            hole7: Hole(number: 16, par: 4, yardage: 426, handicap: 1),
-            hole8: Hole(number: 17, par: 5, yardage: 171, handicap: 17),
-            hole9: Hole(number: 18, par: 4, yardage: 424, handicap: 5))
-        let tee = CourseTee(color: .blue, rating: 71.0, slope: 115, frontNine: frontNine, backNine: backNine)
-        round = Round(tee, nines: [1,0])
-    }
+    
 
 }
