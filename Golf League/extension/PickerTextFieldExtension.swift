462a94c8064becd9a303b2b6bdbcb5450c2d8ec7
AJ Motika <andrew.motika@gm.com>
2017-12-30 19:01:12 -0500

    Removed golf leage files

--- Golf League/extension/PickerTextFieldExtension.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PickerTextFieldExtension.swift
-//  Golf League
-//
-//  Created by Andrew Motika on 12/30/17.
-//  Copyright © 2017 Andrew Motika. All rights reserved.
-//
-
-import Foundation
--- Golf League/extension/PickerTextFieldView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PickerTextFieldView.swift
-//  Golf League
-//
-//  Created by Andrew Motika on 12/30/17.
-//  Copyright © 2017 Andrew Motika. All rights reserved.
-//
-
-import Foundation
